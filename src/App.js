4f918c054f3b5ab2989b773e9a09fa3a652fc9b3
Vasanth Ravichandiran <vasanth913@gmail.com>
2024-01-19 10:33:35 +0530

    Cra&tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-3xl font-bold">
       Namaste Everyone
+      <div>
+        <button className='p-2 m-4 border-gray-200 rounded-lg bg-green-300'> Sample </button>
+      </div>
+      
     </div>
   );
 }
